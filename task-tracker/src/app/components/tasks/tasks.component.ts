e5e5fffd277e72d3faf26fd19ea5a7789c829ca6
lifeKhomola <khomolavhutshilo5@gmail.com>
2021-12-14 11:10:23 +0200

    server up and running

--- task-tracker/src/app/components/tasks/tasks.component.ts
+++ task-tracker/src/app/components/tasks/tasks.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import { TASKS } from 'src/app/mork-task';
+import { TaskService } from 'src/app/services/task.service';
+
 import { Task } from 'src/app/Task';
 @Component({
   selector: 'app-tasks',
@@ -7,10 +8,12 @@ import { Task } from 'src/app/Task';
   styleUrls: ['./tasks.component.css']
 })
 export class TasksComponent implements OnInit {
-  tasks: Task[] = TASKS;
-  constructor() { }
+  tasks: Task[] = [];
+
+  constructor(private taskService: TaskService) { }
 
   ngOnInit(): void {
+    this.taskService.getTasks().subscribe((tasks) => this.tasks = tasks);
   }
 
 }
--- task-tracker/src/app/services/task.service.ts
+++ task-tracker/src/app/services/task.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { TASKS } from 'src/app/mork-task';
+import { Task } from 'src/app/Task';
+import { Observable, observable,of } from 'rxjs';
+ 
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
 
-  constructor() { }
+  constructor(){ }
+  getTasks(): Observable<Task[]> {
+    const tasks=of(TASKS);
+    return tasks;
+  }
 }
